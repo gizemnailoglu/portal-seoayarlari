6cc648a1af8270d1ce701ad0bf80939fbef8660d
gizemnailoglu <gizem.nailoglu@gmail.com>
2019-01-06 04:16:15 +0300

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,95 @@
+<?php
+namespace kouosl\seoayarlari;
+
+use Yii;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\auth\QueryParamAuth;
+use yii\web\HttpException;
+
+
+/**
+ * seoayarlari module definition class
+ */
+class Module extends \kouosl\base\Module
+{
+    public $controllerNamespace = '';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        switch ($this->namespace)
+        {
+            case 'backend':{
+
+            };break;
+            case 'frontend':{
+
+            };break;
+            case 'api':{
+
+                $behaviors['authenticator'] = [
+                    'class' => CompositeAuth::className(),
+                    'authMethods' => [
+                        HttpBasicAuth::className(),
+                        HttpBearerAuth::className(),
+                        QueryParamAuth::className(),
+                    ],
+                ];
+            };break;
+            case 'console':{
+
+            };break;
+            default:{
+                throw new HttpException(500,'behaviors'.$this->namespace);
+            };break;
+        }
+
+        return $behaviors;
+
+    }
+
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['site/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@kouosl/seoayarlari/messages',
+            'fileMap' => [
+                'seoayarlari/seoayarlari' => 'seoayarlari.php',
+            ],
+        ];
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('seoayarlari/'. $category, $message, $params, $language);
+    }
+
+    public static function initRules(){
+
+        return $rules = [
+            [
+                'class' => 'yii\rest\UrlRule',
+                'controller' => [
+                   'seoayarlari/seoayarlaris',
+                ],
+                'tokens' => [
+                    '{id}' => '<id:\\w+>'
+                ],
+                /*'patterns' => [
+                    'GET new-action' => 'new-action'
+                ]*/
+            ],
+
+        ] ;
+
+    }
+}
